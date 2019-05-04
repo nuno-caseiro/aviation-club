630d9e341b8dc8fc9f3d5266da742644ff7dd4e5
Barkhide <nunocas3iro@gmail.com>
2019-05-04 02:30:50 +0100

    coisas

---
+++ app/AeronaveValores.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
+
+class AeronaveValores extends Model
+{
+    protected $table= 'aeronaves_valores';
+
+    protected $fillable= ['minutos','preco'];
+
+
+    public function aeronaves(){
+        return $this->belongsToMany('App\Aeronave');
+    }
+}
