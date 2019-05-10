6903cd56c71b0115530b0ce6d594a06983e341f0
António Gomes <antoniom.gomes10@gmail.com>
2019-05-10 10:10:30 +0100

    dsads

--- app/User.php
+++ app/User.php
@@ -19,7 +19,6 @@ class User extends Authenticatable implements MustVerifyEmail
 
 
     protected $table= 'users';
-    protected $primaryKey = 'id'; // preciso disto?
   //  protected $fillable = [
       //  'name', 'email', 'password','num_socio','nome_informal','tipo','direcao','quotas_pagas','ativo'
    // ]; //nome,email,password , ja vem por defeito da autenticacao
