127a69bb582611311b273464f9597dca5de63c6d
Barkhide <nunocas3iro@gmail.com>
2019-05-16 23:03:07 +0100

    cenas bacanas

--- resources/views/users/editPassword.blade.php
+++ resources/views/users/editPassword.blade.php
@@ -31,6 +31,22 @@
                             {{ csrf_field() }}
                             {{ method_field('PATCH') }}
 
+                            <div class="form-group{{ $errors->has('current_password') ? ' has-error' : '' }}">
+                                <label for="current-password" class="col-md-4 control-label">Current Password</label>
+
+                                <div class="col-md-6">
+                                    <input id="current-password" type="password" class="form-control" name="current_password">
+
+                                    @if ($errors->has('current_password'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('current_password') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+
+
                             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                                 <label for="password" class="col-md-4 control-label">Password</label>
 
@@ -53,19 +69,7 @@
                                 </div>
                             </div>
 
-                            <div class="form-group{{ $errors->has('current_password') ? ' has-error' : '' }}">
-                                <label for="current-password" class="col-md-4 control-label">Current Password</label>
 
-                                <div class="col-md-6">
-                                    <input id="current-password" type="password" class="form-control" name="current_password">
-
-                                    @if ($errors->has('current_password'))
-                                        <span class="help-block">
-                                        <strong>{{ $errors->first('current_password') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
 
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
